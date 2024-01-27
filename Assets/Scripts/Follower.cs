bad69891853a43aa8b910e1cd41fa9cf1e189b1d
MaxRomagnoli <info@maxromagnoli.com>
2024-01-27 11:49:40 +0100

    added animated custom speaker

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -4,6 +4,8 @@ public class Follower : MonoBehaviour
 {
     [SerializeField] private GameObject target;
     [SerializeField] private float speed = 1f;
+    [Tooltip("Distance before follower stop following")]
+    [SerializeField] private float minDistance = 2f;
 
     public void AddSpeed(float _toAdd)
     {
@@ -13,6 +15,15 @@ public class Follower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += (target.transform.position - this.transform.position).normalized * speed * Time.deltaTime;
+        Vector3 direction = target.transform.position - this.transform.position;
+        if(minDistance < 0 || direction.sqrMagnitude < minDistance) { return; }
+        this.transform.position += direction.normalized * speed * Time.deltaTime;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, minDistance);
     }
 }
